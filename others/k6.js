1c55a4780896b204a1354067c90cab14b11c03da
karry0104 <zhen860104@gmail.com>
2023-07-24 10:55:24 +0800

    update code for loading test

--- others/k6.js
+++ others/k6.js
@@ -1,71 +1,79 @@
 import http from "k6/http";
-import { sleep } from "k6";
-import { check } from "k6";
+import { sleep, check } from "k6";
+import { Counter } from "k6/metrics";
 
 import { uuidv4 } from "https://jslib.k6.io/k6-utils/1.4.0/index.js";
 
 export const options = {
-  stages: [
-    { duration: "10s", target: 100 },
-
-    { duration: "20s", target: 0 },
-  ],
+  stages: [{ duration: "5s", target: 5000 }],
 };
 
-const data = {
-  data: {
-    id: 69,
-    randomString: uuidv4(),
-  },
-};
+const id = 69;
+let req1Count = new Counter("req1");
+let req2Count = new Counter("req2");
+let req3Count = new Counter("req3");
 
 export default () => {
-  const url =
-    "https://llibq3igv0.execute-api.ap-northeast-1.amazonaws.com/v1/mes69";
-  const payload = JSON.stringify({
-    id: 69,
-    randomString: uuidv4(),
-  });
-
-  const params = {
-    headers: {
-      "Content-Type": "application/json",
+  const data = {
+    data: {
+      id,
+      randomString: uuidv4(),
     },
   };
 
-  const res = http.post(url, payload, params);
+  const req1 = {
+    method: "POST",
+    url: `https://ssub96p2v2.execute-api.ap-northeast-1.amazonaws.com/v1/mes${id}`,
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  };
 
-  check(res, { "status was 200": (r) => r.status == 200 });
+  const req2 = {
+    method: "POST",
+    url: `https://18jihetbil.execute-api.ap-northeast-1.amazonaws.com/prod/seat/${id}`,
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  };
 
+  const req3 = {
+    method: "POST",
+    url: `https://yzuhyu.com/api/v1/order`,
+    body: JSON.stringify({
+      showSeatId: [108],
+      showId: id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsInVzZXJuYW1lIjoiYWEiLCJlbWFpbCI6ImFhQGdtYWlsLmNvbSIsImlhdCI6MTY4OTk5NDc3OSwiZXhwIjoxNjkwMjEwNzc5fQ.UZV5t93MRjAzIf_fK7TXztdACyRxlHh8bWJwuguzDm4`,
+    },
+  };
+
+  const response = http.post(req1.url, req1.body, req1.headers);
+  //console.log(response.body);
+  check(response, { "status was 200": (r) => r.status == 200 });
+  req1Count.add(1);
+
+  let value = 0;
+  while (value === 0) {
+    sleep(1);
+    const response2 = http.post(req2.url, req2.body, req2.headers);
+    console.log(response2.body);
+    check(response2, { "status was 200": (r) => r.status == 200 });
+    const data = JSON.parse(response2.body);
+    value = data.data.checkKey;
+    req2Count.add(1);
+  }
+
+  const response3 = http.post(req3.url, req3.body, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsInVzZXJuYW1lIjoiYWEiLCJlbWFpbCI6ImFhQGdtYWlsLmNvbSIsImlhdCI6MTY4OTk5NDc3OSwiZXhwIjoxNjkwMjEwNzc5fQ.UZV5t93MRjAzIf_fK7TXztdACyRxlHh8bWJwuguzDm4`,
+    },
+  });
+  check(response3, {
+    "status was 200": (r) =>
+      r.status == 200 || r.status == 400 || r.status == 401,
+  });
+  req3Count.add(1);
   sleep(1);
-  //   const req1 = {
-  //     method: "POST",
-  //     url: "https://llibq3igv0.execute-api.ap-northeast-1.amazonaws.com/v1/mes69",
-  //     body: data,
-  //     params: {
-  //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     },
-  //   };
-  //   const req2 = {
-  //     method: "POST",
-  //     url: "https://8tqvd2l76i.execute-api.ap-northeast-1.amazonaws.com/prod/queue/69",
-  //     params: {
-  //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     },
-  //   };
-
-  //   const req3 = {
-  //     method: "POST",
-  //     url: "https://18jihetbil.execute-api.ap-northeast-1.amazonaws.com/prod/seat/69",
-  //     body: data,
-  //     params: {
-  //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     },
-  //   };
-
-  //   const responses = http.batch([req1, req2, req3]);
-
-  //   check(responses[0], {
-  //     "main page 200": (res) => res.status === 200,
-  //   });
 };
